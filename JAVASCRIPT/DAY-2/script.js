661f9262c7d72d3777118501c4ead5865f3a0eac
Mukesh Singh <mmk606066@gmail.com>
2025-10-06 23:51:45 +0530

    Add files via upload

---
+++ JAVASCRIPT/DAY-2/script.js
@@ -0,0 +1,125 @@
+// let age = 20;
+// let name = "Mukesh";
+// let dob = 2005;
+// let price = 99.99;
+
+// let isonline = true;
+
+
+
+// console.log(typeof age);
+// console.log(typeof name);
+// console.log(typeof isonline);
+// console.log(typeof price);
+
+// console.log(name.lenth);
+
+
+// name = "Ankit";
+
+// age = 30;
+
+// console.log(age)
+// console.log(name)
+
+
+// var city = "NEW DELHI";
+// console.log(city);
+
+// var city = "GORAKPUR";
+
+// console.log(city);
+
+
+
+
+
+
+
+
+// console.log(age);
+// console.log(name);
+// console.log(dob);
+
+
+// if (true) {
+//   var test = "hello";
+// }
+// console.log(test); // Output: hello (accessible outside block)
+
+
+// let name = "Mukesh";
+
+// let name = "Ankit";
+
+
+// const country = "India";
+// country = "USA"; // Error
+
+// const person = {
+//     name : "Mukesh",
+//     age: 20,
+//     city: "New Delhi"
+// }
+
+// person.name="Aditya";
+
+// console.log(person.name);
+// console.log(person);
+
+// const arr = [ 20,"New Delhi","Mukesh"]
+
+// console.log(arr[0]);
+// console.log(arr[1]);
+
+// arr[1]= "Goa";
+
+// console.log(arr[1]);
+
+
+// let msg = "JavaScript";
+// console.log(msg.length); // 10
+// console.log(msg.toLowerCase()); // JAVASCRIPT
+// console.log(msg.slice(4, 9)); // Java
+
+
+// let data;
+
+// console.log(data)
+
+// let result = null;
+// console.log(result); // null
+// console.log(typeof result); // "object" (JavaScript bug)
+
+
+// let id = Symbol("id");
+// let id2 = Symbol("id");
+// console.log(id === id2); // false (symbols are always unique)
+
+
+// let num = 9879457934593495739457934759837593n;
+
+// console.log(num);
+// console.log(typeof num);
+
+// let user = {
+//   name: "Mukesh",
+//   age: 20,
+//   city: "Delhi"
+// };
+// console.log(user.name); // Mukesh
+
+// console.log(user.age);
+
+
+// let fruits = ["apple", "mango","banana"]
+
+// console.log(fruits[2]);
+
+// console.log(fruits.length);
+
+function printhello(){
+    console.log("Hello World!");
+}
+
+printhello();
