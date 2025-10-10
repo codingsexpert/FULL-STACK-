f27c747f788e2596b064204d8b97c483792acf7d
Mukesh Singh <mmk606066@gmail.com>
2025-10-10 11:25:33 +0530

    Add files via upload

---
+++ JAVASCRIPT/DAY-4/script.js
@@ -0,0 +1,138 @@
+// let a = 5; a--
+// let b = 10;
+// b--
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a**b);
+// console.log(a%b);
+// console.log(a);
+// console.log(b);
+
+// let channel = "Codings Expert"
+
+// console.log(channel);
+
+// let a = 10;
+// a += 5
+// a -= 5
+// a *= 5
+// a /= 5
+// a %= 5
+
+// console.log(a);
+
+// let a = 5;
+
+// let b = 10;
+
+// console.log(a !== b);
+
+// console.log(10 > 5 );
+// console.log(10 < 5 );
+// console.log(10 <= 5 );
+// console.log(10 >= 10 );
+ 
+// let mukesh = "name";
+// // let ankit = true;
+
+// // console.log(mukesh || ankit);
+// console.log(typeof mukesh);
+
+
+// let my_age = 14;
+
+// if (my_age >= 18){
+
+//     console.log("You can vote now!")
+   
+    
+// }
+// else{
+//     console.log("You are not 18 now so you can't vote");
+    
+// }
+
+// let marks = 32;
+
+// if(marks >= 90 && marks < 100 ){
+//     console.log("Grade A+ ");
+    
+// }
+// else if (marks >= 80 && marks< 90){
+//     console.log("Grade A");
+    
+// }
+// else if (marks >= 70 && marks< 80){
+//     console.log("Grade B+");
+    
+// }
+// else if (marks >= 60 && marks< 70){
+//     console.log("Grade B");
+    
+// }else if (marks >= 50 && marks< 60){
+//     console.log("Grade C+");
+    
+// }else if (marks >= 40 && marks< 50){
+//     console.log("Grade C");
+    
+// }
+// else if (marks >= 34 && marks< 40){
+//     console.log("Grade D");
+    
+// }
+// else if (marks >= 1 && marks< 34){
+//     console.log("FAIL");
+    
+// }
+
+// let age = 12;
+// let ticket = false;
+// if(age>= 18){
+//     if(ticket == true){
+//         console.log("Aap ab movie dekh sakte ho");
+//     }
+//     else{
+//         console.log("You can't watch movie");  
+//     }
+//     console.log("You are eligible for movie");  
+// }
+// else{
+//     console.log("Aap 18+ nahi ho toh aap entry nahi le sakte PVR me ");
+    
+// }
+
+// for(let i = 1 ; i <= 10; i++){
+//     console.log(i);
+    
+
+// }
+
+// let i = 10;
+
+// while(i >=1 ){
+//     console.log(i);
+//     i--;
+    
+// }
+
+// let i = 1;
+
+// do{
+//     console.log(i*2);
+//     i++;
+    
+// }
+// while( i <= 10){
+    
+// }
+
+// for( let i = 1; i<= 10; i++){
+//     if(i == 6)
+//     // break
+//     continue
+//     console.log(i);
+    
+// }
