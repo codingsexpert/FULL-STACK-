7db5a1afd32643437ffdb116eb88824a01061df6
Mukesh Singh <mmk606066@gmail.com>
2025-10-12 22:32:29 +0530

    Add files via upload

---
+++ JAVASCRIPT/DAY-5/script.js
@@ -0,0 +1,66 @@
+
+// let a = 1/0;
+// let b = 3.44;
+// let c =  -5;
+// console.log(a);
+
+// let name = "Codings Expert"*2;
+
+// console.log(name);
+// let num = 60.6475;
+
+
+// console.log( typeof num.toString());
+
+// console.log( typeof num);
+
+// console.log(num.toFixed(2));
+// console.log(num.toPrecision(2));
+
+
+// let str = "50";
+
+// console.log( typeof Number("67"));
+
+
+
+// let sqr = 6;
+
+// console.log(Math.min(75,56,4,45,45,65,45,));
+
+// console.log(Math.round(4.65));
+// console.log(Math.ceil(5.64));
+
+// console.log(Math.floor(67.90));
+
+// console.log(Math.abs(-20));
+
+// console.log(Math.floor(Math.random()*10 )+1);
+
+// let min =9999, max =1000;
+
+// let value = Math.floor(Math.random()*(max-min + 1000))+min;
+
+// console.log(value);
+
+
+// console.log(Math.floor(Math.random()*(9999- 1000)) +1000) ;
+
+
+// let colors = [ "red", "pink", "green", "orange", "purple"];
+
+// let randomcolor = colors[Math.floor(Math.random()*colors.length)];
+
+// console.log(randomcolor);
+
+
+// let dice = Math.floor(Math.random()*6 +1);
+// console.log(dice);
+console.log((0.1+0.2).toFixed(1));
+
+let num =45.45
+
+console.log(typeof num .toFixed(2));
+
+
+
