55379cbf9639a117e6f91f6e041d5ab1521c7d43
Mukesh Singh <mmk606066@gmail.com>
2025-10-17 18:30:03 +0530

    Add files via upload

---
+++ JAVASCRIPT/DAY-6/script.js
@@ -0,0 +1,101 @@
+
+// let name = "Mukesh";
+// let text = 'My name is "MUKESH" and I am 20 years old'
+
+
+// let phone = "India's No.1 phone"
+
+// let channel = 'Codigs Expert';
+
+
+// let fullname = `Mukesh 
+// sldjflsjdf
+// klsdjflsjdf
+// ksdlkfjas;
+// lkdsjflsjd
+// Singh`;
+
+
+
+// let names = new String ("Mukesh")
+
+// console.log(typeof name);
+
+
+// let channel = 'Codigs Expert';
+
+
+// accesssing method
+// console.log(channel.length);
+// console.log(channel[8]);
+// console.log(channel[0]);
+// console.log(channel.charAt(5));
+
+// conver in lowers case and upper case
+// console.log(channel.toUpperCase());
+// console.log(channel.toLowerCase());
+
+// let name = "Codings Expert ";
+
+// console.log(name.slice(0,5));
+
+// console.log(name.split(""));
+
+// console.log(name.substring(3,7));
+
+// let myself = "My name is Mukesh and I am 20 years old ";
+
+//  let fullname  = "   Mukesh Singh  ";
+// Searching in string
+// console.log(myself.includes("Y"));
+// console.log(myself.indexOf(20));
+// console.log(myself.startsWith("M"));
+// console.log(myself.endsWith("M"));
+
+// Replace and trim method
+
+// console.log(myself.replace(20,30));
+
+// console.log(myself);
+// console.log(fullname);
+// console.log(fullname.trim());
+
+
+// let text = "apple banana grapes";
+
+
+// let fruits = text.split(","); 
+
+// console.log(fruits);
+
+// console.log(fruits.join("|"));
+
+
+// let hi = "Hello ";
+
+// let hello = "hi"
+
+// // console.log(hi.repeat(10));
+// console.log(hi.concat(78));
+
+let name = "Mukesh";
+let age = 30;
+
+let myself = `My name \" ${name} \"is  and I am ${age} years old`;
+
+
+console.log(myself);
+
+
+
+// let a = "apple";
+// let b = "banana";
+// console.log(a < b);
+
+
+
+
+
+
+
+
