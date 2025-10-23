b6717fb5e4295402347838ccf5fd0ab674da20df
Mukesh Singh <mmk606066@gmail.com>
2025-10-23 17:39:08 +0530

    Add files via upload

---
+++ JAVASCRIPT/DAY-8/script.js
@@ -0,0 +1,18 @@
+let user = {
+    name : "Mukesh",
+    age : 29,
+    result:  "Pass",
+    phone :7854903487,
+
+
+}
+// console.log(key.user);
+
+// user.account = 1234;
+
+// console.log(user);
+// delete user.name;
+// console.log(user);
+
+console.log(Object.values(user));
+
