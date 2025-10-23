9abe7008551734f1ce4ae949d2f20e2dd577a845
Mukesh Singh <mmk606066@gmail.com>
2025-10-23 17:39:36 +0530

    Add files via upload

---
+++ JAVASCRIPT/DAY-9/script.js
@@ -0,0 +1,62 @@
+// let marks = [89,78,60,50,57,50];
+
+// console.log(marks);
+// console.log(marks[1]);
+
+
+// for(let i = 0; i<= marks.length; i++){
+//     console.log(marks[i]);
+    
+// }
+
+// for(let i of marks){
+//     console.log(i);
+    
+// }
+// marks.push(90)
+// marks.unshift(50)
+
+// marks.splice(1,2,70,70)
+// marks.pop()
+// marks.shift()
+
+// let marks = [89,78,60,50,57,50];
+
+// let arr = new Array(70,90,78,68,)
+// console.log(arr);
+
+// marks[2] = 66;
+// console.log(marks.length);
+
+// marks.length = 2;
+// console.log(marks);
+// marks.sort()
+// marks.reverse()
+// let grade = [ "A", "B", "C", "D"];
+// let fruits = ["apple","mango","guava"];
+
+// let addarr = marks.concat(grade);
+
+// console.log(...marks,...grade,...fruits);
+
+
+// console.log(addarr);
+
+// console.log(fruits.includes("Apple"));
+// console.log(fruits.toString());
+// console.log(fruits);
+
+// for(let i = 0 ; i<= fruits.length; i++){
+//     console.log(fruits[i]);
+    
+// }
+
+// let fruits = ["apple",["papaya", "kiwi",["banana" ,"litchi"],"cherr"],"mango","guava"];
+
+// console.log(fruits[1][2][1]);
+
+// let value = [800,900,800,600];
+
+// let user = ["mukesh",20, "New Delhi", true ]
+
+// console.log(typeof value);
