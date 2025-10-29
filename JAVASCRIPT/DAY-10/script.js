576d298fb33a7a0ec43b9e0932b766febb4491e5
Mukesh Singh <mmk606066@gmail.com>
2025-10-30 00:06:13 +0530

    Add files via upload

---
+++ JAVASCRIPT/DAY-10/script.js
@@ -0,0 +1,97 @@
+
+// // let name = "chanchal";
+// const student = {
+//     name : "Ankit",
+
+//     address :{
+//         city : "New Delhi",
+//         village : "Saroth"
+//     },
+
+//     college : "merit",
+//     course : "BCA",
+//     id : 898343434,
+//     greet(){
+//         console.log("Hello ",this.name); 
+//     }
+
+
+
+// }
+
+// console.log(student.address.village);
+
+
+
+// student.center = "Desh Bandhu College";
+// delete student.id ;
+// // console.log(student["id"]);
+// // CRUD Operations
+// console.log(student);
+
+// for (let getkey in student){
+//     console.log(getkey,student[getkey]);
+    
+// }
+
+// console.log(student.greet());
+
+
+// let product = [
+//     { name : "iphone",price : 108000},
+//     { name : "charger",price : 4000},
+
+
+// ]
+
+// console.log(product[0].price);
+
+// let car = {
+//     name : "mercedese",
+//     price : "5cr",
+//     color : "yellow"
+
+// }
+
+// // Object.freeze(car)
+// Object.seal(car);
+
+// car.color ="red";
+// // delete car.color;
+
+// console.log(car);
+
+// destrunct karna
+// const person = {
+    
+//     name: "Aman", 
+//     age: 22 
+// };
+
+// let {name,age} = person;
+
+// console.log(name);
+
+let user1 = {
+    name : "sakshi",
+    age : 19,
+    password :{
+        insta : 74384,
+        fb : 78434
+    },
+}
+
+// let user2 ={
+
+//     name : "manav",
+//     age : 20
+// }
+
+// let user2 = user1;
+
+// user2.password.fb = 1111
+
+
+// console.log(user2,user1);
+
+
