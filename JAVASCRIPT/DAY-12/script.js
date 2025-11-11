94800de248fea56bc7d28208fede501e93d86b2e
Mukesh Singh <mmk606066@gmail.com>
2025-11-11 12:49:16 +0530

    Add files via upload

---
+++ JAVASCRIPT/DAY-12/script.js
@@ -0,0 +1,44 @@
+
+
+
+
+// var name = "mukesh"; 
+
+// // var age = 20; 
+
+// // function sum (){
+
+// //     var a = 20 
+// //     var b = 40 
+// //     console.log(a+b);
+    
+// // }
+
+
+
+
+
+// let name = "Mukesh" uninitialised Temporal Dead Zone
+ 
+// const age =  30 uninitialised Temporal Dead Zone
+
+
+
+
+let sum = ()=>{
+
+    let a = 30; 
+    const b  = 20; 
+    console.log(a+b);
+    
+
+}
+
+sum()
+
+
+
+
+
+let name = "mukesh"
+console.log(name);
