37ad6e1342ba0cc5fabbbb3ace647fec8bd7922a
Mukesh Singh <mmk606066@gmail.com>
2026-01-09 16:23:30 +0530

    Add files via upload

---
+++ JAVASCRIPT/DAY-16/script.js
@@ -0,0 +1,6 @@
+// const element = window.document.querySelector("h1");
+// element.textContent="Hello I am changed"
+// element.style.backgroundColor="red"
+
+// const para = document.querySelector("p")
+// para.style.backgroundColor="green"
