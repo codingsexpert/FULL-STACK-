b89e4b4231a9bbc21348e08ecd929a5b8406fad6
Mukesh Singh <mmk606066@gmail.com>
2026-01-11 18:58:42 +0530

    Add files via upload

---
+++ JAVASCRIPT/DAY-17/script.js
@@ -0,0 +1,46 @@
+
+
+// const body = document.querySelector("body")
+// const nameList = document.createElement("ul")
+// const fragment = document.createDocumentFragment();
+
+
+// let arr = ["Manav","Dalip","SOHAN","MAHI","TINA","mahi"]
+
+// for(let names of arr){
+//     const lists = document.createElement("li")
+//     lists.textContent=names
+
+//     fragment.append(lists);
+//     nameList.append(fragment)
+
+// }
+
+
+
+// const list = document.createElement("li")
+// list.textContent="Mohan"
+
+// const list1 = document.createElement("li")
+// list1.textContent="Ajay"
+
+// const list2 = document.createElement("li")
+// list2.textContent="Manav"
+
+// const list3 = document.createElement("li")
+// list3.textContent="Rohan"
+
+// const list4 = document.createElement("li")
+// list4.textContent="Sakshi"
+
+
+// const list5 = document.createElement("li")
+// list5.textContent="Ankita"
+
+
+// nameList.append(list,list1,list2,list3,list4)
+
+// body.append(nameList)
+
+// nameList.children[4].append(list5)
+
